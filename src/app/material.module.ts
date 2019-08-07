e11cff4d2336d4dd857478a31ca7a4f17d5e4013
Stan0men <stan0men@yahoo.com>
2019-08-07 10:32:31 +0300

    start developing in the dev branch

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule]
 })
 export class MaterialModule {
 }
